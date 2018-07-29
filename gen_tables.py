202e5c8aaa17a54416a8546a67b08bfccc708865
Avi Mizrahi <avi1mizrahi@gmail.com>
2018-07-29 18:30:25 +0300

    convert gen_tables.py from python2 to 3
    (using 2to3 tool)

--- gen_tables.py
+++ gen_tables.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 import re
@@ -20,14 +20,14 @@ def process_define(syscalls, text):
     if text.startswith('SYSCALL_DEFINE('):
         m = re.search(r'^SYSCALL_DEFINE\(([^)]+)\)\(([^)]+)\)$', text)
         if not m:
-            print "Unable to parse:", text
+            print("Unable to parse:", text)
             return
         name, args = m.groups()
         types = [s.strip().rsplit(" ", 1)[0] for s in args.split(",")]
     else:
         m = re.search(r'^SYSCALL_DEFINE(\d)\(([^,]+)\s*(?:,\s*([^)]+))?\)$', text)
         if not m:
-            print "Unable to parse:", text
+            print("Unable to parse:", text)
             return
         nargs, name, argstr = m.groups()
         if argstr is not None:
@@ -72,14 +72,14 @@ def parse_type(t):
 def write_output(syscalls_h, types, numbers):
     out = open(syscalls_h, 'w')
 
-    print >>out, "#define MAX_SYSCALL_NUM %d" % (max(numbers.keys()),)
-    print >>out, "const char* syscalls[] = {"
+    print("#define MAX_SYSCALL_NUM %d" % (max(numbers.keys()),), file=out)
+    print("const char* syscalls[] = {", file=out)
     
     for i in range(max(numbers.keys()) + 1):
         if i not in numbers:
-            print >>out, "    \"nosyscall\",";
+            print("    \"nosyscall\",", file=out);
         else:
-            print >>out, "    \"%s\"," % (numbers[i],);
+            print("    \"%s\"," % (numbers[i],), file=out);
 
     #for num in sorted(numbers.keys()):
     #    name = numbers[num]
@@ -95,12 +95,12 @@ def write_output(syscalls_h, types, numbers):
     #    out.write(", ".join([parse_type(t) for t in args] + ["-1"] * (6 - len(args))))
     #    out.write("}},\n");
 
-    print >>out, "};"
+    print("};", file=out)
     out.close()
 
 def main(args):
     if not args:
-        print >>sys.stderr, "Usage: %s /path/to/linux-2.6" % (sys.argv[0],)
+        print("Usage: %s /path/to/linux-2.6" % (sys.argv[0],), file=sys.stderr)
         return 1
     linux_dir = args[0]
     if os.uname()[4] == 'x86_64':
