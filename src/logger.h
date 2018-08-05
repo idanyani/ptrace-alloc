7a44ae28c1ad7d8a0f715b7b6a627ef57cb70cac
Avi Mizrahi <avi1mizrahi@gmail.com>
2018-08-05 12:45:58 +0300

    Logger class

---
+++ src/logger.h
@@ -0,0 +1,37 @@
+#ifndef PTRACE_ALLOC_LOGGER_H
+#define PTRACE_ALLOC_LOGGER_H
+
+#include <iostream>
+
+class Logger {
+  public:
+    enum class Verbosity { ON, OFF}; // can be extended (low-high etc.)
+
+    Logger(std::ostream& out = std::cout) : out_(out), verbosity_(Verbosity::OFF) {}
+
+    template<typename T>
+    Logger& operator<<(const T& t) {
+        if (verbosity_ == Verbosity::ON)
+            out_ << t;
+        return *this;
+    }
+
+    static constexpr class Endl {} endl = Endl();
+
+    Logger& operator<<(Endl) {
+        if (verbosity_ == Verbosity::ON)
+            out_ << std::endl;
+        return *this;
+    }
+
+    Logger& operator<<(Verbosity verbosity) {
+        verbosity_ = verbosity;
+        return *this;
+    }
+
+  private:
+    std::ostream&   out_;
+    Verbosity       verbosity_;
+};
+
+#endif //PTRACE_ALLOC_LOGGER_H
