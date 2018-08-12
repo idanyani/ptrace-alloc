8cf822292f8cd888cc89f1b2b3ed88614597e5de
Avi Mizrahi <avi1mizrahi@gmail.com>
2018-08-12 17:23:05 +0300

    Unit-tests: pipe communication with tracee process. - tracee_mmap was removed

--- unittests/ptrace_unittest.cxx
+++ unittests/ptrace_unittest.cxx
@@ -7,36 +7,93 @@
 
 class PtraceTest : public ::testing::Test {
   protected:
-    PtraceTest() : kill_syscall("kill") {}
+    PtraceTest() : kill_syscall("kill"), command_pipe{-1, -1}, data_pipe{-1, -1} {
+        start_tracee("./tracee");
+    }
+
+    ~PtraceTest() override {
+        closeFd(command_pipe[0]);
+        closeFd(command_pipe[1]);
+        closeFd(data_pipe[0]);
+        closeFd(data_pipe[1]);
+    }
+
+    void sendCommand(char command) {
+        ASSERT_EQ(write(command_pipe[1], &command, sizeof(command)), (ssize_t)sizeof(command));
+
+        static const Syscall read_syscall("read");
+        for (int i = 0; i < 2; ++i) {
+            ASSERT_EQ(read_syscall, p_ptrace->runUntilSyscallGate().first);
+        }
+    }
+
+    template <typename Data>
+    void receiveData(Data& data) {
+        static const Syscall write_syscall("write");
+        for (int i = 0; i < 2; ++i) {
+            ASSERT_EQ(write_syscall, p_ptrace->runUntilSyscallGate().first);
+        }
+
+        ASSERT_EQ(read(data_pipe[0], &data, sizeof(data)), (ssize_t)sizeof(data));
+    }
+
+    std::unique_ptr<Ptrace> p_ptrace;
+    const Syscall           kill_syscall;
+
+  private:
+    static void closeFd(int& fd) {
+        if (fd >= 0)
+            close(fd);
+        fd = -1;
+    }
+
+    void openPipes() {
+        ASSERT_EQ(0, pipe(command_pipe));
+        ASSERT_EQ(0, pipe(data_pipe));
+    }
+
+    void closeUnusedPipeEnds() {
+        closeFd(command_pipe[0]);
+        closeFd(data_pipe[1]);
+    }
 
     void start_tracee(const char* tracee_path) {
         char* args[] = {const_cast<char*>(tracee_path), nullptr};
 
+        openPipes();
+
         p_ptrace.reset(new Ptrace(args[0], args));
 
-        const auto max_tries = 1000;
-        for (auto i = 0; i < max_tries; ++i) {
+        closeUnusedPipeEnds();
+
+        auto max_tries = 1000;
+        while (max_tries--) {
             auto syscall_info = p_ptrace->runUntilSyscallGate();
             ASSERT_EQ(syscall_info.second, Ptrace::SyscallDirection::ENTRY);
 
             syscall_info = p_ptrace->runUntilSyscallGate();
             ASSERT_EQ(syscall_info.second, Ptrace::SyscallDirection::EXIT);
 
-            if (syscall_info.first == kill_syscall)
-                return;
+            if (syscall_info.first == kill_syscall) {
+                break;
+            }
         }
 
-        FAIL() << kill_syscall << " wasn't found in the first " << max_tries << " syscalls";
+        ASSERT_LE(0, max_tries) << kill_syscall << " wasn't found in the first " << max_tries << " syscalls";
+
+        const Syscall close_syscall("close");
+        for (int i = 0; i < 4; ++i) {
+            ASSERT_EQ(close_syscall, p_ptrace->runUntilSyscallGate().first);
+        }
     }
 
-    std::unique_ptr<Ptrace> p_ptrace;
-    const Syscall           kill_syscall;
-
+    int command_pipe[2];
+    int data_pipe   [2];
 };
 
 
 TEST_F(PtraceTest, BasicTest) {
-    start_tracee("./tracee");
+    sendCommand(0);
 
     auto syscall_info = p_ptrace->runUntilSyscallGate();
     EXPECT_EQ(std::string(syscall_info.first.toString()) , "close");
@@ -54,7 +111,7 @@ TEST_F(PtraceTest, BasicTest) {
 }
 
 TEST_F(PtraceTest, MmapHijackTest) {
-    start_tracee("./tracee_mmap");
+    sendCommand(1);
 
     auto syscall_info = p_ptrace->runUntilSyscallGate();
     EXPECT_EQ(std::string(syscall_info.first.toString()) , "mmap");
@@ -67,6 +124,10 @@ TEST_F(PtraceTest, MmapHijackTest) {
     syscall_info = p_ptrace->runUntilSyscallGate();
     EXPECT_EQ(syscall_info.first , getpid_syscall);
     ASSERT_EQ(syscall_info.second , Ptrace::SyscallDirection::EXIT);
+
+    pid_t res;
+    receiveData(res);
+    ASSERT_EQ(p_ptrace->getChildPid(), res);
 
     auto exit_status = p_ptrace->runUntilExit();
     ASSERT_TRUE(exit_status.second);
--- unittests/tracee.cxx
+++ unittests/tracee.cxx
@@ -2,11 +2,45 @@
 #include <unistd.h>
 #include <csignal>
 #include <iostream>
+#include <sys/mman.h>
+
+static const int command_fd = 3;
+static const int result_fd  = 6;
+
+int test0() {
+    close(0);
+    return 0;
+}
+
+int test1() {
+    const auto res = (pid_t)(intptr_t) mmap(nullptr,
+                                            4,
+                                            PROT_READ | PROT_WRITE,
+                                            MAP_PRIVATE | MAP_ANONYMOUS,
+                                            -1,
+                                            0);
+
+    return write(result_fd, &res, sizeof(res)) != sizeof(res);
+}
 
 int main() {
 
-    if (kill(getpid(), 0) != 0) return -1;
-    close(0);
+    if (kill(getpid(), 0) != 0)  // this for caching main's start
+        return -1;
 
-    return 0;
+    close(4);
+    close(5);
+
+    char command;
+    if (read(command_fd, &command, sizeof(command)) < 1)
+        return -1;
+
+    switch (command) {
+        case 0:
+            return test0();
+        case 1:
+            return test1();
+        default:
+            return -1;
+    }
 }
